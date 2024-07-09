52db617729607fd011152e55c7e0a3a133ded80c
유종화 <sola201@naver.com>
2024-07-10 00:50:20 +0900

    로그인 기능 구현 완료

--- shop/src/main/java/com/apple/shop/blog/blog.java
+++ shop/src/main/java/com/apple/shop/blog/blog.java
@@ -1,5 +1,6 @@
 package com.apple.shop.blog;
 
+import com.apple.shop.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,9 +15,9 @@ import java.time.LocalDate;
 @ToString
 @Getter
 @Setter
-public class blog {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class blog extends BaseEntity {
+//    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
     private String title;
     private LocalDate date;
 }
